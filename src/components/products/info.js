395846478000034a38044dcbe6990d354976696d
rizwanali1493 <alishakir1493@gmail.com>
2021-09-01 17:38:26 +0500

    procefileupdate

--- src/components/products/info.js
+++ src/components/products/info.js
@@ -11,6 +11,7 @@ import { getCartTotal } from "../../services";
 import { toast } from "react-toastify";
 import moment from "moment";
 // import PatternLock from "react-pattern-lock";
+// import PatternLock from "react-pattern-lock";
 import Demo from "./pattern";
 class Info extends Component {
   constructor(props) {
